255b227629e2602f3cde23a32538223e188c4e6d
pacoccino <pac@opac.me>
2024-02-04 10:08:14 +0100

    revalidate

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { refreshCache } from './actions';
+
+export default function Page() {
+    return (
+        <div>
+            <h2>Admin Page</h2>
+            <button onClick={() => refreshCache()}>Revalidate</button>
+        </div>
+    );
+}
