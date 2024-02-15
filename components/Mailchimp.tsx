b4cf224a8bb1a5d68219aaa0b044ff65d519ab12
pacoccino <pac@opac.me>
2024-02-15 15:38:18 +0100

    mc tags

--- components/Mailchimp.tsx
+++ components/Mailchimp.tsx
@@ -3,7 +3,12 @@ import { Button } from "./ui/button";
 export default function Mailchimp({ className }: { className?: string }) {
     return (
         <div className="flex flex-col items-center py-4 bg-persian-green-700 text-white">
-            <form action="https://proton.us12.list-manage.com/subscribe/post?u=1a6dc02d62ba4c7ff0d03c95a&amp;id=8c5f545e7c&amp;f_id=004b4de0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
+            <form
+                action="https://proton.us12.list-manage.com/subscribe/post?u=1a6dc02d62ba4c7ff0d03c95a&amp;id=8c5f545e7c&amp;f_id=004b4de0f0"
+                method="post" id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                target="_blank"
+            >
                 <div className="flex flex-col items-center space-y-2 w-full max-w-64">
                     <h2 className="mb-2">Rester informé</h2>
                     <input
@@ -15,6 +20,7 @@ export default function Mailchimp({ className }: { className?: string }) {
                         required
                         minLength={4}
                     />
+                    <input type="hidden" name="tags" value="10694776" />
                     <div aria-hidden="true" className="absolute hidden l-[-5000px]">
                         <input type="text" name="b_1a6dc02d62ba4c7ff0d03c95a_8c5f545e7c" tabIndex={-1} value="" />
                     </div>
